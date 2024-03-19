52d3e9ff8dfac8bad9c5fe63863323fc802f1f6e
RianAina <hei.rianaina@gmail.com>
2024-03-19 15:10:32 +0300

    Package controller fixed

---
+++ src/main/java/com/prog4/FinalWallet/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.prog4.FinalWallet.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "This is the test";
+    }
+}
